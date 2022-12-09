f8e9200e7d2809973b3fc62a3948bd4fb5debbfa
snehilms <snehilms@gmail.com>
2022-12-10 01:23:21 +0530

    updated message to ui

--- drone/views.py
+++ drone/views.py
@@ -1,46 +1,77 @@
 from django.shortcuts import render
 import pyrebase
 import requests
- 
-config = {
-    "apiKey": "AIzaSyBhIVn3prbiF0XWw5xqV2UtnSFVcm6Qocg",
-    "authDomain": "drone-system-iot.firebaseapp.com",
-    "databaseURL": "https://drone-system-iot-default-rtdb.firebaseio.com",
-    "projectId": "drone-system-iot",
-    "storageBucket": "drone-system-iot.appspot.com",
-    "messagingSenderId": "101869826123",
-    "appId": "1:101869826123:web:d11da3d9d6566fd54b5dce",
-    "measurementId": "G-H6MH5YHNXY"
 
-  }
-firebase=pyrebase.initialize_app(config)
-authe = firebase.auth()
-database=firebase.database()
-from django.http import HttpResponse
+import sys, errno  
+# from  firebase import firebase
 
-# Create your views here.
+import firebase_admin
+from firebase_admin import credentials,db
+from threading import Thread
+
+cred = credentials.Certificate("D:/droneNetworking/drone-system-iot-firebase-adminsdk-selsr-36021455d8.json")
+firebase_admin.initialize_app(cred,{
+	'databaseURL':"https://drone-system-iot-default-rtdb.firebaseio.com"
+	})
+# firebase = firebase.FirebaseApplication('https://drone-system-iot-default-rtdb.firebaseio.com',None)
 
 
-def index(request):
+# config = {
+# "apiKey": "AIzaSyBhIVn3prbiF0XWw5xqV2UtnSFVcm6Qocg",
+#   "authDomain": "drone-system-iot.firebaseapp.com",
+#   "databaseURL": "https://drone-system-iot-default-rtdb.firebaseio.com",
+#   "projectId": "drone-system-iot",
+#   "storageBucket": "drone-system-iot.appspot.com",
+#   "messagingSenderId": "101869826123",
+#   "appId": "1:101869826123:web:d11da3d9d6566fd54b5dce",
+#   "measurementId": "G-H6MH5YHNXY"
+#   }
+# firebase=pyrebase.initialize_app(config)
+# authe = firebase.auth()
+# database=firebase.database()
 
-    # latitude = database.child('Data').child('latitude').stream(stream_handler)
-    # longitude = database.child('Data').child('longitude').stream(stream_handler)
-    # data = database.child('gps').child('coordinates').get()
-    # latitude = 
-    longitude = database.child('Data').child('longitude').get().val()
+ref = db.reference('gps')
+# data=dict()
+# def on_data_added(event):
+#     print(event.data)
+#     latitude = data['Latitude']
+#     longitude = data['Longitude']
+    
+    # data = event.data.popitem()[1]
+    # print("data: ",data['Latitude'])
+    
 
-    return render(request,"drone/index.html",{"latitude":latitude,"longitude":longitude,"latitude_direction":latitude_direction,"longitude_direction":longitude_direction})
+# def listen_for_data():
+#     try:
+#         ref.listen(on_data_added)
+#     except KeyboardInterrupt:
+#         print("Stopping data listening...")
 
 # def index(request):
-#     return render(request,'drone/index.html')
+#     listen_thread = Thread(target=listen_for_data)
+#     listen_thread.start()
+#     return render(request,"drone/index.html")
+def index(request):
+    try:
+        data = ref.order_by_child('timestamp').limit_to_last(1).get()
+        data_fields = data[list(data.keys())[0]]
+        latitude = data_fields['Latitude']
+        longitude = data_fields['Longitude']
+        timestamp = data_fields['timestamp']
+        print("Latitude:", latitude)
+        print("Longitude:", longitude)
+        print('Timestamp:',timestamp)
+        # return final_data
+        # ref.listen(on_data_added)
+    except KeyboardInterrupt:
+        print("Stopping data listening...")
+    return render(request,"drone/index.html",{"Latitude":latitude,"Longitude":longitude,"timestamp":timestamp})
+    
 
-def check_drone_status(request):
+def check_drone_status(request): 
     return render(request,'drone/check_drone_status.html')
 
 
-def stream_handler(message):
-    # Print the updated data to the console
-    print(message["data"])
 
 def home_page(request):
     return render(request,'drone/home.html')
